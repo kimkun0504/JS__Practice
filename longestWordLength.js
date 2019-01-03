ae9f78bce7f0d8418d644fc4eb6fb448343ace3a
Kun Kim <41974064+kimkun0504@users.noreply.github.com>
2019-01-03 17:35:58 -0500

    Update longestWordLength.js

--- longestWordLength.js
+++ longestWordLength.js
@@ -1,11 +1,12 @@
 // Given a string, find the length of the longest word in the string
-
+/*
 Input: String
 Output: Number (Length of the longest word)
 
 Step 1: Split the string(sentence) into separate word elements in an array
 Step 2: Iterate through the array and store the longest word in a var
 Step 3: reuturn the stored longest word
+*/
 
 // Using the ES5 syntax
 function longestWordLength (str) {
